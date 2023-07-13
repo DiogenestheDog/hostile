71f5bd531d5a23bf3332666fff73dffdb3ceb686
Chloe Guthrie <chloeruthguthrie@gmail.com>
2023-07-13 14:53:10 -0700

    initiated db

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
-require('dotenv').config()
-console.log(process.env)
 
+require('dotenv').config();
+
+console.log(process.env);
 console.log(process.env.MAP_KEY);
+
 const app = express();
 let port = process.env.PORT;
 
 if (port == null || port == "") {
-  port = 8000;
+    port = "8000";
 }
 
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -16,8 +18,9 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/api/mapkey', (req, res) => {
     console.log("I'm in the route");
     const mapKey = process.env.MAP_KEY;
-    res.json({mapKey});
-})
+    res.json({ mapKey });
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
